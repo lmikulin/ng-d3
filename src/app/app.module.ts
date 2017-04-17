19a1b88617c863241452c4105c6826523f5e877f
Liana Mikulin <lianamikulin@Lianas-MacBook-Pro.local>
2017-04-17 14:44:03 -0700

    added angular routing and a simple css-grid example with overlayed day/name columns and rows

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ResourceGridComponent } from './components/resource-grid/resource-grid.component';
+import { CssGridComponent } from './components/css-grid/css-grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ResourceGridComponent
+    ResourceGridComponent,
+    CssGridComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {path: 'svg', component: ResourceGridComponent},
+      {path: 'grid', component: CssGridComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
